89b7ee2b0210ac7d670628274102efb6d80c70e9
Satori <shakeebmajid@ufl.edu>
2017-12-25 18:36:54 -0500

    basic blockchain verification is done along with display of time to mine block

--- majicoinPrototype.js
+++ majicoinPrototype.js
@@ -31,7 +31,7 @@ class Block {
 class BlockChain {
   constructor() {
     this.chain = [this.createGenesisBlock()];
-    this.difficulty = 5;
+    this.difficulty = 18;
 
   }
 
@@ -43,16 +43,45 @@ class BlockChain {
   }
 
   addBlock(newBlock) {
+    var start = new Date();
     newBlock.previousHash = this.getLatestBlock().hash;
     newBlock.mineBlock(this.difficulty);
     this.chain.push(newBlock);
+    var end = new Date();
+    var millisecondsElapsed = end - start;
+    var secondsElapsed = millisecondsElapsed / 1000;
+    console.log("Time to mine block: " + secondsElapsed + "s")
 
   }
 
+  isChainValid() {
+
+    for(let i = 1; i < this.chain.length; ++i) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if(currentBlock.hash !== currentBlock.calcHash() || currentBlock.hash.substring(0, this.difficulty) !== Array(this.difficulty + 1).join("0")) {
+        return false;
+      }
+
+      if(currentBlock.previousHash !== previousBlock.hash) {
+        console.log("previous block hash: " + previousBlock.hash.toString());
+        return false;
+      }
+
+    }
+
+    return true;
+
+  }
 }
 
 let majicoin = new BlockChain();
-majicoin.addBlock(new Block(1, "12/24/2017", {amount: 4}));
+console.log("Mining Block 1...");
+majicoin.addBlock(new Block(1, "12/24/2017", {amount: 24}));
 
-
-console.log("This is a hash:" + SHA256("hi this is a hash").toString())
+if(majicoin.isChainValid()) {
+  console.log("Blockchain is valid");
+} else {
+  console.log("Blockchain is not valid");
+}
